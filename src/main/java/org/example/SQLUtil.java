d5813e2591eca3cc1b1918f057350fc97b1036ca
Jack White <ataev0461@gmail.com>
2026-08-20 19:58:28 +0300

    правки

--- src/main/java/org/example/SQLUtil.java
+++ src/main/java/org/example/SQLUtil.java
@@ -7,28 +7,42 @@ import java.sql.SQLException;
 
 public class SQLUtil {
     private final String JDBC_URL;
-    private final String JDBC_PASSWORD;
     private final String JDBC_USERNAME;
+    private final String JDBC_PASSWORD;
 
-    public SQLUtil(String JDBC_URL, String JDBC_PASSWORD, String JDBC_USERNAME) {
+    public SQLUtil(String JDBC_URL, String JDBC_USERNAME, String JDBC_PASSWORD) {
         this.JDBC_URL = JDBC_URL;
-        this.JDBC_PASSWORD = JDBC_PASSWORD;
         this.JDBC_USERNAME = JDBC_USERNAME;
+        this.JDBC_PASSWORD = JDBC_PASSWORD;
     }
 
 
-    public void sendSQLRequest(String sql) {
-        try(Connection connection = getConnection()) {
-            connection.createStatement().execute(sql);
+    public String sendSQLRequest(String sql) {
+        try(
+                Connection connection = getConnection();
+                var statement = connection.createStatement();
+        ) {
+            boolean hasResultSet = statement.execute(sql);
+
+            if (hasResultSet) {
+                try(var resultSet = statement.getResultSet()) {
+                    if (resultSet.next()) {
+                        return resultSet.getString(1);
+                    }
+                }
+            }
+            return null;
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+
     private Connection getConnection() {
         try {
             return DriverManager.getConnection(
-                    JDBC_URL, JDBC_PASSWORD, JDBC_USERNAME
+                    JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/test/java/PostgresTest.java
+++ src/test/java/PostgresTest.java
@@ -1,14 +1,13 @@
 import org.example.ENVUtil;
 import org.example.SQLUtil;
 import org.example.UserRequestSQL;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PostgresTest {
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.2")
@@ -25,14 +24,17 @@ public class PostgresTest {
     }
 
     @Test
+    @Order(1)
     public void shouldInsertUser() {
         sqlUtil.sendSQLRequest(UserRequestSQL.insertUserTable("ivan", "ivan@mail.ru", 25));
+        sqlUtil.sendSQLRequest(UserRequestSQL.insertUserTable("anton", "ivan@mail.ru", 25));
     }
 
     @Test
+    @Order(2)
     public void shouldGETUser() {
         String result = sqlUtil.sendSQLRequest(UserRequestSQL.getUserNameTable("anton"));
-        Assertions.assertEquals("ivan",  result);
+        Assertions.assertEquals("anton",  result);
     }
 
 }
